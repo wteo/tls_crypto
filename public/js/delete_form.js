3581fe003af0240008e08d8ecf74d51fcff3e0fe
Wendy Teo <wendyteo.wy@gmail.com>
2023-09-17 14:37:00 +1000

    added modal popup

--- public/js/delete_form.js
+++ public/js/delete_form.js
@@ -1,3 +1,37 @@
-function confirmDelete() {
-    alert("Are you sure you want to delete this location?");
-}
+document.addEventListener('DOMContentLoaded', (event) => {
+    const deleteButtons = document.querySelectorAll('.region__admin-delete-button');
+    const deleteModal = document.getElementById('deleteModal');
+    const closeModal = document.querySelector('.modal-close');
+    const confirmDeleteButton = document.getElementById('confirmDeleteButton');
+    const cancelDeleteButton = document.getElementById('cancelDeleteButton');
+    let selectedForm;
+
+    deleteButtons.forEach(deleteButton => {
+        deleteButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            selectedForm = e.target.closest('form');
+            deleteModal.style.display = "block";
+        });
+    });
+
+    closeModal.addEventListener('click', () => {
+        deleteModal.style.display = "none";
+    });
+
+    confirmDeleteButton.addEventListener('click', () => {
+        if (selectedForm) {
+            selectedForm.submit();
+        }
+        deleteModal.style.display = "none";
+    });
+
+    cancelDeleteButton.addEventListener('click', () => {
+        deleteModal.style.display = "none";
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == deleteModal) {
+            deleteModal.style.display = "none";
+        }
+    });
+});
