7d3c298165742e4b66970e2630c5a4db54421d1f
Wendy Teo <wendyteo.wy@gmail.com>
2023-10-08 06:34:24 +1100

    updated navigation

--- public/js/nav_header.js
+++ public/js/nav_header.js
@@ -1,16 +1,17 @@
 // Toggle menu on hamburger click
 document.querySelector('.nav__hamburger').addEventListener('click', function() {
     const menu = document.querySelector('.nav__menu');
-    menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'flex' : 'none';
+    menu.classList.toggle('active');
 });
+
 
 // Reset menu on window resize
 window.addEventListener('resize', function() {
     const menu = document.querySelector('.nav__menu');
     if (window.innerWidth >= 751) {
-        menu.style.display = 'flex'; // Show menu on desktop
+        menu.classList.remove('active');
     } else {
-        menu.style.display = 'none'; // Hide menu on mobile
+        menu.classList.remove('active'); 
     }
 });
 
