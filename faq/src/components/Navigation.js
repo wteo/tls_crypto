637a19e3ab7d004a21c7e42017815a548a7ac1a1
Wendy Teo <wendyteo.wy@gmail.com>
2023-10-22 16:05:19 +1100

    added hamburger

--- faq/src/components/Navigation.js
+++ faq/src/components/Navigation.js
@@ -6,17 +6,20 @@ function Navigation() {
 
     return (
         <header>
+            <button className={styles.nav__hamburger}>
+                &#9776;
+            </button>
         <nav className={styles.nav}>
-          <ul className={styles.nav__menu}>
-              <li className={styles.nav__icon__wrapper}>
-                  <a href="/">
-                      <img className={styles.nav__icon__image} src="https://i0.wp.com/thelazysociety.com/wp-content/uploads/2020/02/The-Lazy-Society-logo.png?fit=220%2C194&ssl=1" alt="TLS Logo"/>
-                  </a>
-              </li>
-              <li className={styles.nav__menu__title}>About Us</li>
-              <li className={styles.nav__menu__title}>The Team</li>
-              <li className={styles.nav__menu__title}>FAQ</li>
-          </ul>
+            <ul className={styles.nav__menu}>
+                <li className={styles.nav__icon__wrapper}>
+                    <a href="/">
+                        <img className={styles.nav__icon__image} src="https://i0.wp.com/thelazysociety.com/wp-content/uploads/2020/02/The-Lazy-Society-logo.png?fit=220%2C194&ssl=1" alt="TLS Logo"/>
+                    </a>
+                </li>
+                <li className={styles.nav__menu__title}>About Us</li>
+                <li className={styles.nav__menu__title}>The Team</li>
+                <li className={styles.nav__menu__title}>FAQ</li>
+            </ul>
         </nav>
       </header>
     );
