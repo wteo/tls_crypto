6eb27dafd97172ac2e1df1b87caeb2b68638df73
Wendy Teo <wendyteo.wy@gmail.com>
2023-10-22 17:33:35 +1100

    updated for mobile navigation

--- faq/src/components/Navigation.js
+++ faq/src/components/Navigation.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styles from './Navigation.module.css';
 
 function Navigation() {
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(prevState => !prevState);
+    }
+
     return (
         <header>
-            <button className={styles.nav__hamburger}>
-                &#9776;
+            <button className={styles.nav__hamburger} onClick={toggleMenu}>
+                { isMenuOpen ? 'X' : '≡' }
             </button>
-        <nav className={styles.nav}>
-            <ul className={styles.nav__menu}>
-                <li className={styles.nav__icon__wrapper}>
+            <div className={styles.nav__top__bar}>
+                <div className={styles.nav__icon__image__mobile}>
                     <a href="/">
                         <img className={styles.nav__icon__image} src="https://i0.wp.com/thelazysociety.com/wp-content/uploads/2020/02/The-Lazy-Society-logo.png?fit=220%2C194&ssl=1" alt="TLS Logo"/>
-                    </a>
-                </li>
-                <li className={styles.nav__menu__title}>About Us</li>
-                <li className={styles.nav__menu__title}>The Team</li>
-                <li className={styles.nav__menu__title}>FAQ</li>
-            </ul>
-        </nav>
+                        </a>
+                </div>
+            </div>
+            <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}>
+                <ul className={styles.nav__menu}>
+                    <li className={styles.nav__icon__wrapper}>
+                        <a href="/">
+                            <img className={styles.nav__icon__image} src="https://i0.wp.com/thelazysociety.com/wp-content/uploads/2020/02/The-Lazy-Society-logo.png?fit=220%2C194&ssl=1" alt="TLS Logo"/>
+                        </a>
+                    </li>
+                    <li className={styles.nav__menu__title} onClick={toggleMenu}>About Us</li>
+                    <li className={styles.nav__menu__title} onClick={toggleMenu}>The Team</li>
+                    <li className={styles.nav__menu__title} onClick={toggleMenu}>FAQ</li>
+                </ul>
+            </nav>
+            <div 
+                onClick={toggleMenu} 
+                className={styles.nav__overlay} 
+                style={{ display: isMenuOpen ? 'block' : 'none' }} 
+            />
       </header>
     );
 }
